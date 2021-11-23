29810cbc6d363e177f5e8b2b47b8e49b2d0c5b3a
zekisert <zsert1963@gmail.com>
2021-11-24 02:20:39 +0300

    DbContext ve Connectionstring Eklendi.

---
+++ NetSatis.Entities/Context/NetSatisContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using NetSatis.Entities.Tables;
+
+namespace NetSatis.Entities.Context
+{
+    public class NetSatisContext:DbContext
+    {
+        public DbSet<Stok> Stoklar { get; set; }
+        public DbSet<Cari> Cariler { get; set; }
+        public DbSet<Fis> Fisler { get; set; }
+        public DbSet<StokHareket> StokHareketleri { get; set; }
+        public DbSet<KasaHareket> KasaHareketleri { get; set; }
+        public DbSet<Depo> Depolar { get; set; }
+        public DbSet<Kasa> Kasalar { get; set; }
+        public DbSet<OdemeTuru> OdemeTurleri { get; set; }
+        public DbSet<Tanim> Tanimlar { get; set; }
+
+
+    }
+}
